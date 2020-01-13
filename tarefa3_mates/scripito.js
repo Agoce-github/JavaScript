52922aa3c73931d7c3397ccb8068d5ef800fb5b5
Agoce-github <56600410+Agoce-github@users.noreply.github.com>
2020-01-13 22:23:53 +0100

    Update scripito.js

--- tarefa3_mates/scripito.js
+++ tarefa3_mates/scripito.js
@@ -13,12 +13,21 @@ var v1=16;
 
 /* Raíz cuadrada */
 var r1=Math.sqrt(v1);
-alert(r1);
+alert("Raíz cuadrada=" + r1);
 
 /* Número elevado al cuadrado */
 var r2=Math.pow(5,2);
-alert(r2);
+alert("Número elevado al cuadrado=" + r2);
 
 /* Número PI sin decimales */
 var r3=Math.PI;
-alert(r3);
+var r4=Math.round(r3);
+alert("Número PI sin decimales=" + r4);
+
+/* Número mínimo */
+var r5=53;
+var r6=82;
+alert("Número mínimo=" + Math.min(r5,r6));
+
+/* Función de número aleatorio entre 0 y 3 */
+alert("Número aleatorio=" + Math.random(0,3));
