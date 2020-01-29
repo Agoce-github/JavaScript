b10bbe49b062e1fda4534e10c1a54c1683f666d5
Agoce-github <56600410+Agoce-github@users.noreply.github.com>
2020-01-29 09:56:12 +0100

    Update scripito.js

--- tarefa10_euro2yens/scripito.js
+++ tarefa10_euro2yens/scripito.js
@@ -4,49 +4,53 @@
 	Usado: 
 */
 
-var fr1;
-fr1 = prompt("¿Cuál es el producto?");
+var newpro = "¿Has comprado algo más?";
+var rnew;
+rnew = true;
+while (rnew == true) {
 
-var fr2;
-fr2 = prompt("¿Qué valor tiene "+ fr1 +"?");
+	var fr1;
+	fr1 = prompt("¿Cuál es el producto?");
 
-var fr3;
-fr3 = prompt("¿En qué moneda has pagado la cantidad de "+ fr2 +"?");
+	var fr2;
+	fr2 = prompt("¿Qué valor tiene "+ fr1 +"?");
 
-switch(fr3)
-{
-	case 'euro':
-	case 'euros':
-	case 'Euro':
-	case 'Euros':
-	case '€':
-		var pre1;
-		pre1 = parseInt(fr2)*120.29;
-		document.write("Los "+fr2+"€ que has pagado por "+fr1+" equivalen a "+pre1+" Yens(¥).");
-	break;
-	case 'yen':
-	case 'yens':
-	case 'Yen':
-	case 'Yens':
-	case '¥':
-		var pre2;
-		pre2 = parseInt(fr2)/120.29;
-		document.write("Los "+fr2+"¥ que has pagado por "+fr1+" equivalen a "+pre2+" Euros(€).");
-	break;
+	var fr3;
+	fr3 = prompt("¿En qué moneda has pagado la cantidad de "+ fr2 +"?");
 
-	default:
-		document.write('Lo siento solo trabajamos con Euros (€) o Yens (¥), por favor, inténtelo de nuevo.');
-	break;
-};
+		switch(fr3)
+		{
+			case 'euro':
+			case 'euros':
+			case 'Euro':
+			case 'Euros':
+			case '€':
+				var pre1;
+				pre1 = parseFloat(fr2)*120.29;
+				pre11 = pre1.toFixed(2);
+				document.write("<br/><br/>Los "+fr2+"€ que has pagado por "+fr1+" equivalen a "+pre11+" Yens(¥).");
+					document.write("<br/><br/>El valor de "+fr1+" es de:");
+					document.write("<br/>Euros(€)= "+pre11);
+					document.write("<br/>Yens(¥)= "+pre1);
+			break;
+			case 'yen':
+			case 'yens':
+			case 'Yen':
+			case 'Yens':
+			case '¥':
+				var pre2;
+				pre2 = parseFloat(fr2)/120.29;
+					pre22 = pre2.toFixed(2);
+				document.write("<br/><br/>Los "+fr2+"¥ que has pagado por "+fr1+" equivalen a "+pre22+" Euros(€).");
+					document.write("<br/><br/>El valor de "+fr1+" es de:");
+					document.write("<br/>Euros(€)= "+pre22);
+					document.write("<br/>Yens(¥)= "+pre2);
+			break;
 
-document.write("<br/><br/>El valor de "+fr1+" es de:");
-document.write("<br/>Euros(€)= ");
-document.write("<br/>Yens(¥)= ");
-
-
-///ANOTACIONES
-document.write("<br/><br/><br/>PROFE, no he podido completar el ejercico al completo.");
-document.write("<br/>No termino de comprender los bucles y las funciones. He echo lo que he podido.");
-document.write("<br/>Espero poder asimilar el conocimiento de JavaScript pronto para acabar esta práctica.");
-document.write("<br/><br/>Me voy a dormir.");
+			default:
+				document.write('<br/><br/>Lo siento solo trabajamos con Euros (€) o Yens (¥), por favor, inténtelo de nuevo.');
+			break;
+		}
+	rnew = confirm(newpro);
+}
 
