b41f6eb6e12e810fa8e26df0219cf9d0e2896e9c
Agoce-github <56600410+Agoce-github@users.noreply.github.com>
2020-02-04 20:32:25 +0100

    Update scripito.js

--- tarefa3_mates/scripito.js
+++ tarefa3_mates/scripito.js
@@ -4,19 +4,18 @@
 	Usado: 
 */
 
-var moito = 9 ; /* Creamos la variable */
-var raiz = Math.sqrt(moito); /* Math.sqrt(x) hace la raíz cuadrada de "x" */
-alert(raiz); /* Muestra el resultado de la variable "raiz" */
-
 /* Ejercicio */
-var v1=16;
+var v1=16.2345;
+var v2=8.0001;
+var v3=0.0932;
+var v4=2.1005;
 
 /* Raíz cuadrada */
 var r1=Math.sqrt(v1);
 alert("Raíz cuadrada=" + r1);
 
 /* Número elevado al cuadrado */
-var r2=Math.pow(5,2);
+var r2=Math.pow(v2,2);
 alert("Número elevado al cuadrado=" + r2);
 
 /* Número PI sin decimales */
@@ -25,9 +24,7 @@ var r4=Math.round(r3);
 alert("Número PI sin decimales=" + r4);
 
 /* Número mínimo */
-var r5=53;
-var r6=82;
-alert("Número mínimo=" + Math.min(r5,r6));
+alert("Número mínimo=" + Math.min(v3,v4));
 
 /* Función de número aleatorio entre 0 y 3 */
 alert("Número aleatorio=" + Math.random(0,3));
